ef506d6df903511eae990848986018970adf9fd0
Susantha Kumara <dksusantha@outlook.com>
2024-10-15 14:56:46 +0530

    grpc-blog

---
+++ grpc-blog/client/Program.cs
@@ -0,0 +1,107 @@
+﻿using Blog;
+using Grpc.Core;
+
+const string target = "127.0.0.1:50052";
+
+Channel channel = new Channel("localhost", 50052, ChannelCredentials.Insecure);
+
+await channel.ConnectAsync().ContinueWith((task) =>
+{
+    if (task.Status == TaskStatus.RanToCompletion)
+        Console.WriteLine("The client connected successfully");
+});
+
+var client = new BlogService.BlogServiceClient(channel);
+
+//var newBlog = CreateBlog(client);
+//ReadBlog(client);
+//UpdateBlog(client, newBlog);
+//DeleteBlog(client, newBlog);
+await ListBlog(client);
+channel.ShutdownAsync().Wait();
+Console.ReadKey();
+
+static Blog.Blog CreateBlog(BlogService.BlogServiceClient client)
+{
+    var response = client.CreateBlog(new CreateBlogRequest()
+    {
+        Blog = new Blog.Blog()
+        {
+            AuthorId = "Susantha",
+            Title = "New blog!",
+            Content = "Hello world, this is the new blog"
+        }
+    });
+    Console.WriteLine("The blog " + response.Blog.Id + " was created !");
+
+    return response.Blog;
+}
+
+static void ReadBlog(BlogService.BlogServiceClient client)
+{
+    try
+    {
+        var response = client.ReadBlog(new ReadBlogRequest()
+        {
+            BlogId = "66fa9e5685491b8f28fab6a2"
+        });
+        Console.WriteLine(response.Blog.ToString());
+    }
+    catch (RpcException e)
+    {
+        Console.WriteLine(e.Status.Detail);
+    }
+}
+
+static void UpdateBlog(BlogService.BlogServiceClient client, Blog.Blog blog)
+{
+    try
+    {
+        blog.AuthorId = "Updated Author";
+        blog.Title = "Updated Title";
+        blog.Content = "Updated Content";
+
+        var response = client.UpdateBlog(new UpdateBlogRequest()
+        {
+            Blog = blog
+        });
+
+        Console.WriteLine(response.Blog.ToString());
+    }
+    catch (RpcException e)
+    {
+        Console.WriteLine(e.Status.StatusCode);
+    }
+}
+
+static void DeleteBlog(BlogService.BlogServiceClient client, Blog.Blog blog)
+{
+    try
+    {
+        var response = client.DeleteBlog(new DeleteBlogRequest() { BlogId = blog.Id });
+
+        Console.WriteLine("The blog with id " + response.BlogId + " was deleted");
+    }
+    catch (RpcException e)
+    {
+        Console.WriteLine(e.Status.StatusCode);
+    }
+}
+
+static async Task ListBlog(BlogService.BlogServiceClient client)
+{
+    try
+    {
+        var response = client.ListBlog(new ListBlogRequest() { });
+
+        while (await response.ResponseStream.MoveNext())
+        {
+            await Console.Out.WriteLineAsync(response.ResponseStream.Current.Blog.ToString());
+        }
+        
+    }
+    catch (RpcException e)
+    {
+        Console.WriteLine(e.Status.Detail);
+    }
+}
---
+++ grpc-blog/server/BlogServiceImpl.cs
@@ -0,0 +1,125 @@
+﻿using Blog;
+using Grpc.Core;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using static Blog.BlogService;
+
+namespace server
+{
+    public class BlogServiceImpl : BlogServiceBase
+    {
+        private static MongoClient mongoClient = new MongoClient("mongodb://localhost:27017");
+        private static IMongoDatabase mongoDatabase = mongoClient.GetDatabase("mydb");
+        private static IMongoCollection<BsonDocument> mongoCollection = mongoDatabase.GetCollection<BsonDocument>("blog");
+
+        public override Task<CreateBlogResponse> CreateBlog(CreateBlogRequest request, ServerCallContext context)
+        {
+            var blog = request.Blog;
+            BsonDocument doc = new BsonDocument("author_id", blog.AuthorId)
+                                                .Add("title", blog.Title)
+                                                .Add("content", blog.Content);
+
+            mongoCollection.InsertOne(doc);
+
+            String id = doc.GetValue("_id").ToString();
+
+            blog.Id = id;
+
+            return Task.FromResult(new CreateBlogResponse()
+            {
+                Blog = blog
+            });
+        }
+
+
+        public override async Task<ReadBlogResponse> ReadBlog(ReadBlogRequest request, ServerCallContext context)
+        {
+            var blogId = request.BlogId;
+
+            var filter = new FilterDefinitionBuilder<BsonDocument>().Eq("_id", new ObjectId(blogId));
+            var result = mongoCollection.Find(filter).FirstOrDefault();
+
+            if (result == null)
+                throw new RpcException(new Status(StatusCode.NotFound, "The blod id " + blogId + " wasn't find"));
+
+            Blog.Blog blog = new Blog.Blog()
+            {
+                AuthorId = result.GetValue("author_id").AsString,
+                Title = result.GetValue("title").AsString,
+                Content = result.GetValue("content").AsString
+            };
+
+            return new ReadBlogResponse() { Blog = blog };
+        }
+
+        public override async Task<UpdateBlogResponse> UpdateBlog(UpdateBlogRequest request, ServerCallContext context)
+        {
+            var blogId = request.Blog.Id;
+
+            var filter = new FilterDefinitionBuilder<BsonDocument>().Eq("_id", new ObjectId(blogId));
+            var result = mongoCollection.Find(filter).FirstOrDefault();
+
+            if (result == null)
+                throw new RpcException(new Status(StatusCode.NotFound, "The blod id " + blogId + " wasn't find"));
+
+            var doc = new BsonDocument("author_id", request.Blog.AuthorId)
+                                                .Add("title", request.Blog.Title)
+                                               .Add("content", request.Blog.Content);
+
+            mongoCollection.ReplaceOne(filter, doc);
+
+            Blog.Blog blog = new Blog.Blog()
+            {
+                AuthorId = result.GetValue("author_id").AsString,
+                Title = result.GetValue("title").AsString,
+                Content = result.GetValue("content").AsString
+            };
+
+            blog.Id = blogId;
+
+            return new UpdateBlogResponse() { Blog = blog };    
+        }
+
+        public override async Task<DeleteBlogResponse> DeleteBlog(DeleteBlogRequest request, ServerCallContext context)
+        {
+            var blogId = request.BlogId;
+
+            var filter = new FilterDefinitionBuilder<BsonDocument>().Eq("_id", new ObjectId(blogId));
+
+            var result = mongoCollection.DeleteOne(filter);
+
+            if (result.DeletedCount == 0)
+                throw new RpcException(new Status(StatusCode.NotFound, "The blod id " + blogId + " wasn't find"));
+
+            return new DeleteBlogResponse() { BlogId = blogId };
+        }
+
+        public override async Task ListBlog(ListBlogRequest request, IServerStreamWriter<ListBlogResponse> responseStream, ServerCallContext context)
+        {
+            var filter = new FilterDefinitionBuilder<BsonDocument>().Empty;
+
+            var result = mongoCollection.Find(filter);
+
+            foreach (var item in result.ToList())
+            {
+                await responseStream.WriteAsync(new ListBlogResponse()
+                {
+                    Blog = new Blog.Blog()
+                    {
+                        Id = item.GetValue("_id").ToString(),
+                        AuthorId = item.GetValue("author_id").AsString,
+                        Title = item.GetValue("title").AsString,
+                        Content = item.GetValue("content").AsString
+                    }
+                });
+            }
+        }
+    }
+
+}
