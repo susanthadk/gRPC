aa3939bf3acdd5e5884ada72e9d30d736eeb8083
Susantha Kumara <dksusantha@outlook.com>
2024-10-15 14:52:39 +0530

    compute-average

---
+++ compute-average/client/Program.cs
@@ -0,0 +1,29 @@
+﻿using Average;
+using Grpc.Core;
+
+const string target = "127.0.0.1:50052";
+
+Channel channel = new Channel(target, ChannelCredentials.Insecure);
+
+await channel.ConnectAsync().ContinueWith((task) =>
+{
+    if (task.Status == TaskStatus.RanToCompletion)
+        Console.WriteLine("The client connected successfully");
+});
+
+var client = new AverageService.AverageServiceClient(channel);
+var stream = client.ComputeAverage();
+
+foreach (int number in Enumerable.Range(1, 4))
+{
+    var request = new AverageRequest() { Number = number };
+    await stream.RequestStream.WriteAsync(request);
+}
+
+await stream.RequestStream.CompleteAsync();
+var response = await stream.ResponseAsync;
+
+Console.WriteLine(response.Result);
+
+channel.ShutdownAsync().Wait();
+Console.ReadKey();
---
+++ compute-average/server/AverageServiceImpl.cs
@@ -0,0 +1,28 @@
+﻿using Average;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Average.AverageService;
+
+namespace server
+{
+    public class AverageServiceImpl : AverageServiceBase
+    {
+        public override async Task<AverageResponse> ComputeAverage(IAsyncStreamReader<AverageRequest> requestStream, ServerCallContext context)
+        {
+            int total = 0;
+            double result = 0.0;
+
+            while (await requestStream.MoveNext())
+            {
+                result += requestStream.Current.Number;
+                total++;
+            }
+
+            return new AverageResponse() { Result = result / total };
+        }
+    }
+}
