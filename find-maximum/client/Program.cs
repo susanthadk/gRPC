f8a91df033bd00cb79928264d4bead631b59b4a3
Susantha Kumara <dksusantha@outlook.com>
2024-10-15 14:54:29 +0530

    find-maximum

---
+++ find-maximum/client/Program.cs
@@ -0,0 +1,36 @@
+﻿using Grpc.Core;
+using Max;
+
+const string target = "127.0.0.1:50052";
+
+Channel channel = new Channel(target, ChannelCredentials.Insecure);
+
+await channel.ConnectAsync().ContinueWith((task) =>
+{
+    if (task.Status == TaskStatus.RanToCompletion)
+        Console.WriteLine("The client connected successfully");
+});
+
+var client = new FindMaxService.FindMaxServiceClient(channel);
+var stream = client.FindMaximum();
+
+int[] numbers = { 1, 5, 3, 6, 2, 20 };
+foreach (int number in numbers)
+{
+    await stream.RequestStream.WriteAsync(new FindMaxRequest() { Number = number});
+}
+
+var responseReaderTask = Task.Run(async () =>
+{
+    while (await stream.ResponseStream.MoveNext())
+    {
+        Console.WriteLine(stream.ResponseStream.Current.Max);
+        //await Console.Out.WriteLineAsync(stream.ResponseStream.Current.Max.ToString());
+    }
+});
+
+await stream.RequestStream.CompleteAsync();
+await responseReaderTask;
+
+channel.ShutdownAsync().Wait();
+Console.ReadKey();
---
+++ find-maximum/server/FindMaxServiceImpl.cs
@@ -0,0 +1,28 @@
+﻿using Grpc.Core;
+using Max;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Max.FindMaxService;
+
+namespace server
+{
+    public class FindMaxServiceImpl : FindMaxServiceBase
+    {
+        public override async Task FindMaximum(IAsyncStreamReader<FindMaxRequest> requestStream, IServerStreamWriter<FindMaxResponse> responseStream, ServerCallContext context)
+        {
+            int? max = null;
+
+            while (await requestStream.MoveNext())
+            {
+                if(max == null || max < requestStream.Current.Number)
+                {
+                    max = requestStream.Current.Number;
+                    await responseStream.WriteAsync(new FindMaxResponse() { Max = max.Value });
+                }
+            }
+        }
+    }
+}
